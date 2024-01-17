ab24965579284ecd4cbd834294fadc93ca5deb64
janinocencio <janinocencio@gmail.com>
2024-01-17 16:27:28 +1100

    Change gameboard.create to a function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const gameboard = (function () {
-    const create = new Array(9);
+    const create = function () {
+        return new Array(9);
+    };
     const update = function (board, marker, location) {
-        return board[location] = marker;       
+        board[location] = marker;       
     };
     return { create, update };
 })();
